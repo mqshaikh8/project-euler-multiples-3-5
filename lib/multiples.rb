8d216939819fb2ecb911c4701e369413bddc9fe4
Mohammed Shaikh <46161085+mqshaikh8@users.noreply.github.com>
2021-09-22 16:47:07 -0400

    commit tree 82bf8ee012236df3b7795246cfdd4161e49670cb

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+def collect_multiples(data)
+    arr = []
+    for i in 1...data do
+       if i % 3 === 0 || i % 5 === 0
+        arr << i
+       end
+    end
+    arr
+end
+
+def sum_multiples(data)
+    collect_multiples(data).inject(:+)
+end
+# puts collect_multiples(100)
